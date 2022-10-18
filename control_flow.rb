4ba3cc4ed12ad53ccebb5c7319a9692f0cdde7d7
diana nthenya odhiambo <dianaodhiambo1096@gmail.com>
2022-10-18 23:02:37 +0300

    completed assignment

--- control_flow.rb
+++ control_flow.rb
@@ -1,16 +1,51 @@
 def admin_login(username, password)
-  # your code here
+  if (username == "admin" || username == "ADMIN") && password == "12345"
+    "Access granted"
+  else
+    "Access denied"
+  end
 end
 
 def hows_the_weather(temperature)
-  # your code here
-end
+  if temperature<40
+     "It's brisk out there!"
+  elsif  temperature >= 40 && temperature <= 65
+    "It's a little chilly out there!"
+  elsif temperature>85
+    "It's too dang hot out there!"
+  else
+    "It's perfect out there!"
+  end
+end  
+puts hows_the_weather(30)
 
-def fizzbuzz(num)
-  # your code here
-end
+def fizzbuzz(number)
+  if number%3==0 && number%5==0
+    "FizzBuzz"
+  elsif number%3==0
+    "Fizz"  
+  elsif number%5==0
+    "Buzz"
+  else 
+    number 
+  end 
+end    
+  
+puts fizzbuzz(15)
 
 def calculator(operation, num1, num2)
-  # your code here
+  if operation == '+'
+    num1 + num2
+  elsif operation == '-'
+    num1 - num2
+  elsif operation == '*'
+    num1 * num2
+  elsif operation == '/'
+    num1 / num2
+  else 
+    print 'Invalid operation!'
+  end
 end
+
+calculator('test',2,3)
 
